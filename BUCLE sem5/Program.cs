d3a951d3c440a1e826d54d7ab54463186bfe0587
Adonay66 <Zeladaadonay7@gmail.com>
2023-02-23 21:53:46 -0600

    Creacion de bucle

---
+++ BUCLE sem5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Creacion de bucle
+Console.WriteLine("Ingresa el primer valor:");
+int inicio = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese el valor final");
+    int final = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Los numeros pares en el rango[{0},{1}]son:", inicio, final);
+        for (int i = inicio; i <= final; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
